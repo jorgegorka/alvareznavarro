8e514fd05fb18567ad59b7b1f934e59c0f89b519
Jorge Alvarez <jorge@alvareznavarro.es>
2018-07-07 19:44:20 +0200

    Use link instead of a.

--- src/components/page-navigation.js
+++ src/components/page-navigation.js
@@ -5,19 +5,19 @@ const PageNavigation = ({ pathContext }) => {
   const { group, index, first, last, pageCount } = pathContext;
   console.log(first);
 
-  const previousUrl = index - 1 == 1 ? "/" : `/${(index - 1).toString()}`;
+  const previousUrl = index - 1 == 1 ? "/" : (index - 1).toString();
   const nextUrl = (index + 1).toString();
 
   return (
     <nav className="pagination" role="navigation">
       { !first &&
         <div className="previousLink">
-          <a className="newer-posts" href={previousUrl}>&larr; <span className="hide">Previous Posts</span></a>
+          <Link className="newer-posts" to={previousUrl}>&larr; <span className="hide">Previous Posts</span></Link>
         </div>
       }
       { !last &&
         <div className="nextLink">
-          <a className="older-posts" href={nextUrl}><span className="hide">Next Posts</span> &rarr;</a>
+          <Link className="older-posts" to={nextUrl}><span className="hide">Next Posts</span> &rarr;</Link>
         </div>
       }
     </nav>
