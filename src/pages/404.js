0b1fe0ce4442b668917cc053b0f6e25902d76100
Jorge Alvarez <jorge@alvareznavarro.es>
2019-07-02 09:46:29 +0200

    Update 404 text.

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,9 @@ import Link from 'gatsby-link'
 const NotFoundPage = () => (
   <div>
     <h1>Page not found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>
+      I've searched everywhere but I couldn't find the page you are looking for.
+    </p>
     <Link to="/">Go back to the homepage.</Link>
   </div>
 )
